72993b2a398c40008db6cb7032b6266e703df5db
albertyitzep <alberttitzep123@gmail.com>
2024-06-13 17:29:06 -0600

    [ADD] test inicial a base de datos

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,16 @@
 const app = require('./app');
 const dotenv = require('dotenv');
-
+const db = require('./config/db');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
+db.getConnection()
+  .then(() => {
+    console.log('Database connected');
+    app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
+  })
+  .catch((err) => {
+    console.log('Database connection error', err);
+  });
