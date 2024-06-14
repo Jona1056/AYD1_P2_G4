fa4058de7fefed67ae030857642ce508a3126e36
zenthic22 <roberchac75@gmail.com>
2024-06-14 14:44:07 -0600

    vistas terminadas

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Registro';
-<<<<<<< HEAD
 import VistaMedico from './pages/GestionCitasMedico';
 import VistaCitas from './pages/Citas';
 import VistaEmail from './pages/CorreoCancelado';
-
-=======
+import VistaHorariosMedico from './pages/HorariosMedico';
 import Doctores from './pages/Doctor'
 import Horario from './pages/horarios';
->>>>>>> develop
+import VistaHistorialMedico from './pages/HistorialCitasMedico';
+import VistaPerfilMedico from './pages/EditarMedico';
+
 function App() {
   return (
     <>
@@ -17,14 +17,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/Register' element={<Register />} />
-<<<<<<< HEAD
           <Route path='/GestionCitas' element={<VistaMedico/>} />
           <Route path='/Citas' element={<VistaCitas/>} />
           <Route path='/Email' element={<VistaEmail/>} />
-=======
           <Route path='/Doctores' element={<Doctores />} />
           <Route path='/Horario' element={<Horario />} />
->>>>>>> develop
+          <Route path='/horarios' element={<VistaHorariosMedico/>} />
+          <Route path='/historial-citas' element={<VistaHistorialMedico/>} />
+          <Route path='/actualizar-perfil' element={<VistaPerfilMedico/>} />
         </Routes>
       </Router>
     </>
