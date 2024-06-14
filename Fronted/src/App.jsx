8c1535abc146e289112fe0a28f3e66cde10345d1
zenthic22 <roberchac75@gmail.com>
2024-06-13 23:34:15 -0600

    faltan 3 vistas

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,7 +1,9 @@
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Registro';
+import VistaMedico from './pages/GestionCitasMedico';
+import VistaCitas from './pages/Citas';
+import VistaEmail from './pages/CorreoCancelado';
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/Register' element={<Register />} />
+          <Route path='/GestionCitas' element={<VistaMedico/>} />
+          <Route path='/Citas' element={<VistaCitas/>} />
+          <Route path='/Email' element={<VistaEmail/>} />
         </Routes>
       </Router>
     </>
